81f2447cedfe1774f018114b44914d84abcca9ef
Matthew Bourque <bourque@stsci.edu>
2017-02-22 16:36:45 -0500

    Initial commit of setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = 'acsql',
+    description = 'The Advanced Camera for Surveys Quicklook Project',
+    url = 'https://github.com/bourque/acsql.git',
+    author = 'Matthew Bourque',
+    author_email = 'bourque@stsci.edu',
+    keywords = ['astronomy'],
+    classifiers = ['Programming Language :: Python'],
+    packages = find_packages(),
+    install_requires = [],
+    version = 0.0
+    )
