d2d1e819c70761fc6c14cc5fff4901449892268c
Matthew Bourque <bourque@stsci.edu>
2017-03-04 15:26:48 -0500

    Added convenient reset_database module, which does exactly what it is called.

---
+++ acsql/database/reset_database.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+"""Reset all tables in the acsql database.
+
+Authors
+-------
+    Matthew Bourque, 2017
+
+Use
+---
+    This script is intended to be used in the command line:
+    >>> python reset_database.py
+"""
+
+from acsql.database.database_interface import base
+from acsql.utils.utils import SETTINGS
+
+
+if __name__ == '__main__':
+
+    prompt = ('About to reset all tables for database instance {}. Do you'
+              'wish to proceed? (y/n)\n'.format(SETTINGS['connection_string']))
+    response = input(prompt)
+
+    if response.lower() == 'y':
+        print('Resetting database.')
+        base.metadata.drop_all()
+        base.metadata.create_all()
