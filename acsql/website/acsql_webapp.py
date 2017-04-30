3016206ef6cc1a61f3f9d0cd8255e213e9037bcd
Matthew Bourque <bourque@stsci.edu>
2017-04-29 21:41:08 -0400

    Implemented templates for 404 and 500 html errors.

--- acsql/website/acsql_webapp.py
+++ acsql/website/acsql_webapp.py
@@ -4,12 +4,55 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+def handle_500(trace):
+    """Handle 500 error.
+
+    Parameters
+    ----------
+    trace : str
+        The traceback of the error.
+
+    Returns
+    -------
+    template : obj
+        The ``500.html`` template.
+    """
+
+    trace_html = trace.replace('\n', '<br>')
+
+    return render_template('500.html', trace_html=trace_html)
+
+
 @app.route('/')
 def main():
-    """
+    """Generates the ``acsql`` website homepage.
+
+    Returns
+    -------
+    template : obj
+        The ``index.html`` template.
     """
 
     return render_template('index.html')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Redirects any nonexistent URL to 404 page.
+
+    Parameters
+    ----------
+    error : obj
+        The ``error`` thrown.
+
+    Returns
+    -------
+    template : obj
+        The ``404.html`` template.
+    """
+
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
