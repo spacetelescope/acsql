3759f66926bf600048198282852ff0cb4b34514d
Matthew Bourque <bourque@stsci.edu>
2017-09-03 18:41:19 -0400

    Added figure that shows the number of records in each database table.

--- paper/database_by_table.py
+++ paper/database_by_table.py
@@ -57,32 +57,62 @@ if __name__ == '__main__':
     sbc_class_objs = [getattr(database_interface, table) for table in sbc_tables]
     wfc_class_objs = [getattr(database_interface, table) for table in wfc_tables]
 
+    # Determine number of records in each table
     hrc_num_records = []
     for table in hrc_class_objs:
+        print('Querying {}'.format(table))
         hrc_num_records.append(session.query(table).count())
 
     sbc_num_records = []
     for table in sbc_class_objs:
+        print('Querying {}'.format(table))
         sbc_num_records.append(session.query(table).count())
 
     wfc_num_records = []
     for table in wfc_class_objs:
+        print('Querying {}'.format(table))
         wfc_num_records.append(session.query(table).count())
 
-    print(wfc_num_records)
+    # Get number of records for master and datasets tables
+    master_num_records = session.query((getattr(database_interface, 'Master'))).count()
+    datasets_num_records = session.query((getattr(database_interface, 'Datasets'))).count()
 
+    # Aggregate results into two lists
+    table_names = wfc_tables
+    table_names.extend(hrc_tables)
+    table_names.extend(sbc_tables)
+    table_names.extend(['Datasets', 'Master'])
 
-    # plt.style.use('bmh')
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111)
-    # ax.bar(np.arange(len(filetypes)), num_files, align='center', color='green')
-    # ax.set_ylabel('# of files')
-    # ax.set_xlim((-1, len(filetypes)))
-    # ax.set_xticks(np.arange(len(filetypes)))
-    # ax.set_xticklabels(filetypes)
-    # ax.tick_params(axis=u'both', which=u'both', length=0)
-    # ax.grid('off')
-    # plt.tight_layout()
-    # plt.savefig('figures/num_files_by_filetype.png')
+    num_records = wfc_num_records
+    num_records.extend(hrc_num_records)
+    num_records.extend(sbc_num_records)
+    num_records.extend([datasets_num_records, master_num_records])
 
-    # print(len(all_files))
+    # Build list of colors
+    colors = ['blue' for item in wfc_tables]
+    colors.extend(['green' for item in hrc_tables])
+    colors.extend(['red' for item in sbc_tables])
+    colors.extend(['orange', 'orange'])
+
+    # Reverse the list so that plot is more aesthetically pleasing
+    table_names = table_names[::-1]
+    num_records = num_records[::-1]
+    colors = colors[::-1]
+
+    # Make the plot
+    plt.style.use('bmh')
+    plt.rcParams['font.size'] = 10
+    fig = plt.figure(figsize=(8,20))
+    ax = fig.add_subplot(111)
+    ax.barh(np.arange(len(table_names)), num_records, align='center', color=colors)
+    ax.set_xlabel('# of records')
+    ax.set_ylim((-1, len(table_names)))
+    ax.set_yticks(np.arange(len(table_names)))
+    ax.set_yticklabels(table_names)
+    ax.tick_params(axis=u'both', which=u'both', length=0)
+    ax.yaxis.grid(False)
+    ax.xaxis.grid(True)
+    plt.tight_layout()
+    plt.savefig('figures/database_records.png')
+
+    print(sum(num_records))
